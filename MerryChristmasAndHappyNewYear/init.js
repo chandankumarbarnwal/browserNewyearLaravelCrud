1f59d40d922e5ce6e0553622fbebd06dc9a15630
Chandan Kumar Barnwal <chandanbarnwal111@gmail.com>
2019-12-02 08:37:11 +0530

    Getting value of name variable from url

--- MerryChristmasAndHappyNewYear/init.js
+++ MerryChristmasAndHappyNewYear/init.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-   $('.footerbtn').attr('href','whatsapp://send?text=Bhool Jao Bite Hua Kal, Dil Me Bsa Lo Ane Wale Pal. Khushiyan Lekar Aayega Ane Wala kal. Naye Saal Ki Dhero Shubhkamnaye Ke Sath.! ★★♡Touch this Blue Line  Enter Your Name and See Magic♡ ★★👉http://wishingyear.co/?name='+a);
+   $('.footerbtn').attr('href','whatsapp://send?text=Bhool Jao Bite Hua Kal, Dil Me Bsa Lo Ane Wale Pal. Khushiyan Lekar Aayega Ane Wala kal. Naye Saal Ki Dhero Shubhkamnaye Ke Sath.! â˜…â˜…â™¡Touch this Blue Line  Enter Your Name and See Magicâ™¡ â˜…â˜…ðŸ‘‰http://wishingyear.co/?name='+a);
 
-
-let searchParams = new URLSearchParams(window.location.search)
+ $('.footerbtn1').attr('href','  https://www.facebook.com/sharer/sharer.php?u=http://wishingyear.co/?name='+a);
+ 
+ 
+ let searchParams = new URLSearchParams(window.location.search)
 
 let param = searchParams.get('name');
 
